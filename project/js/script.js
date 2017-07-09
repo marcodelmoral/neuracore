9331e8a39375cb8aac9ebdbeb38e719ccde050f0
Marco Julio <marcojulioarg@gmail.com>
2017-07-09 13:07:52 -0500

    front

---
+++ project/js/script.js
@@ -0,0 +1,11 @@
+function openChat(){
+	var chat = document.getElementById('chat');
+
+	chat.style.visibility ="visible";
+}
+
+function hideChat(){
+	var chat = document.getElementById('chat');
+
+	chat.style.visibility ="hidden";
+}
